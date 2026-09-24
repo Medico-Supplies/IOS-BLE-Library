b1273434787c09c4ba2a1d4b23b0cc3029bb2285
Medico-Supplies <114607748+Medico-Supplies@users.noreply.github.com>
2026-09-24 11:42:27 -0700

    Change IOS-CoreBluetooth-Mock dependency URL
    Updated dependency URL for IOS-CoreBluetooth-Mock to a new repository.

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         .library(name: "iOS-BLE-Library-Mock", targets: ["iOS-BLE-Library-Mock"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nordicsemi/IOS-CoreBluetooth-Mock.git",
-                 .upToNextMajor(from: "1.0.6")
+        .package(url: "https://github.com/Medico-Supplies/IOS-CoreBluetooth-Mock.git",
+                 .exact(from: "1.0.6")
         ),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.5.0"),
     ],
